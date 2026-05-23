589c1ab33688421cced0dc21464e631b65d8d40c
dmitry76543 <zabava-sharik@mail.ru>
2026-05-23 19:40:40 +0300

    Начальная загрузка

---
+++ _next/static/chunks/02vnpd~228zty.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,83131,t=>{t.v(l=>Promise.all(["static/chunks/0pxl2__s4-8vq.js"].map(l=>t.l(l))).then(()=>l(76914)))}]);
